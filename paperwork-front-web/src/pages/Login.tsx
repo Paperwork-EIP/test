2330def56db7e5eab0ef31a7f0cc847b665de51b
Austin-William Lo <austin-william.lo@epitech.eu>
2022-09-07 10:28:39 +0900

    test sonarcloud

---
+++ paperwork-front-web/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginContent from '../container/Login/LoginContent';
+
+const LoginPage = () => {
+
+    return (
+        <LoginContent/>
+    );
+}
+
+export default LoginPage;
---
+++ paperwork-front-web/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Register from '../container/Register/Register';
+
+const RegisterPage = () => {
+
+    return (
+        <Register/>
+    );
+}
+
+export default RegisterPage;
